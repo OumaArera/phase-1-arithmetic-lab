3b727684f82b5018000766adc16cfb93a6d3d533
OumaArera <johnouma999@gmail.com>
2023-12-13 07:45:53 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+console.log(multiply); 
+
+let random = Math.floor(Math.random() * 1000) + 1;
+
+const num3 = 44;
+const num4 = 5;
+
+const mod = num3 % num4;
+console.log(mod);
+
+let max;
+max = Math.max(5, 10, 20, 3);
+
